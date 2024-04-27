11fcbeb1ef8796c6d3c3a24cbf8909a1f5ac7ac3
KoinuKage <michaleq21@gmail.com>
2024-04-27 19:35:34 +0200

    Small fixes and improvements

--- src/KoinuKage/DeploymentCRReduction/DeploymentCRReduction.java
+++ src/KoinuKage/DeploymentCRReduction/DeploymentCRReduction.java
@@ -5,29 +5,20 @@ import com.fs.starfarer.api.Global;
 import org.magiclib.util.MagicSettings;
 import com.fs.starfarer.api.combat.ShipHullSpecAPI;
 
-import java.util.Iterator;
-import java.util.List;
-
 public class DeploymentCRReduction extends BaseModPlugin {
     float crReductionMultiplier = MagicSettings.getFloat("DeploymentCRReduction", "crReductionMultiplier");
 
-    public void ReduceDeploymentCR (ShipHullSpecAPI ship)
-    {
-        if (crReductionMultiplier < 0)
-            crReductionMultiplier = 0;
-        if (crReductionMultiplier > 100)
-            crReductionMultiplier = 100;
-        ship.setCRToDeploy(ship.getCRToDeploy() * crReductionMultiplier);
-    }
-
     @Override
-    public void onApplicationLoad() {
-
-
-        for (Iterator iteration = Global.getSettings().getAllShipHullSpecs().iterator(); iteration.hasNext(); )
-        {
-            final ShipHullSpecAPI ship = (ShipHullSpecAPI) iteration.next();
+    public void onApplicationLoad(){
+        for (final ShipHullSpecAPI ship : Global.getSettings().getAllShipHullSpecs()) {
             ReduceDeploymentCR(ship);
         }
+    }
+
+    public void ReduceDeploymentCR (ShipHullSpecAPI ship)
+    {
+        if (crReductionMultiplier < 0) crReductionMultiplier = 0;
+        if (crReductionMultiplier > 100) crReductionMultiplier = 100;
+        ship.setCRToDeploy(ship.getCRToDeploy() * crReductionMultiplier);
     }
 }
