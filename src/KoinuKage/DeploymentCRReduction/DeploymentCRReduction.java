ee412ab6a2cace9780cd7a5c73197f9a670b541e
KoinuKage <michaleq21@gmail.com>
2024-04-30 21:53:09 +0200

    Fix for massive supplies cost, added a second parameter to additionaly manipulate the supplies recovery.

--- src/KoinuKage/DeploymentCRReduction/DeploymentCRReduction.java
+++ src/KoinuKage/DeploymentCRReduction/DeploymentCRReduction.java
@@ -5,8 +5,11 @@ import com.fs.starfarer.api.Global;
 import org.magiclib.util.MagicSettings;
 import com.fs.starfarer.api.combat.ShipHullSpecAPI;
 
+import static java.lang.Math.floor;
+
 public class DeploymentCRReduction extends BaseModPlugin {
     float crReductionMultiplier = MagicSettings.getFloat("DeploymentCRReduction", "crReductionMultiplier");
+    float suppliesRecoveryMultiplier = MagicSettings.getFloat("DeploymentCRReduction", "suppliesRecoveryMultiplier");
 
     @Override
     public void onApplicationLoad(){
@@ -19,6 +22,15 @@ public class DeploymentCRReduction extends BaseModPlugin {
     {
         if (crReductionMultiplier < 0) crReductionMultiplier = 0;
         if (crReductionMultiplier > 100) crReductionMultiplier = 100;
-        ship.setCRToDeploy(ship.getCRToDeploy() * crReductionMultiplier);
+        if (((ship.getCRToDeploy() * crReductionMultiplier) <= 1) && (crReductionMultiplier > 0))
+        {
+            ship.setCRToDeploy(1);
+            ship.setSuppliesToRecover(1);
+        }
+        else
+        {
+            ship.setCRToDeploy((float)floor(ship.getCRToDeploy() * crReductionMultiplier));
+            ship.setSuppliesToRecover((float)floor(ship.getSuppliesToRecover() * (crReductionMultiplier * suppliesRecoveryMultiplier)));
+        }
     }
 }
